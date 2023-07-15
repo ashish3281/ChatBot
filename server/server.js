4c0e98bbf2c3521e767dbaabbcb831a980c940c3
ashish3281 <ashishgoyal3281@gmail.com>
2023-07-15 12:06:37 +0530

    first comit

---
+++ server/server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CryptoSphere!',
+  });
+});
+
+app.post('/', async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: prompt,
+      max_tokens: 1000,
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text.trim(),
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error || 'Something went wrong');
+  }
+});
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
